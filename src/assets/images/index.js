295fe83ca03faac6eab8f4981989a4923b825253
mostafa-meerzad <71199539+mostafa-meerzad@users.noreply.github.com>
2023-10-08 18:23:15 +0430

    manage images and icons and isolate image import/exports

---
+++ src/assets/images/index.js
@@ -0,0 +1,19 @@
+import imgDesktop1 from "./desktop-image-hero-1.jpg";
+import imgDesktop2 from "./desktop-image-hero-2.jpg";
+import imgDesktop3 from "./desktop-image-hero-3.jpg";
+import imgMobile1 from "./mobile-image-hero-1.jpg";
+import imgMobile2 from "./mobile-image-hero-2.jpg";
+import imgMobile3 from "./mobile-image-hero-3.jpg";
+import imgAboutDark from "./image-about-dark.jpg";
+import imgAboutLight from "./image-about-light.jpg";
+
+export {
+  imgDesktop1,
+  imgDesktop2,
+  imgDesktop3,
+  imgMobile1,
+  imgMobile2,
+  imgMobile3,
+  imgAboutDark,
+  imgAboutLight,
+};
